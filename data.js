c2bdc440c13a6de92547cc4b077e63acd5bed018
Nenad Misic <nenad.misic@typhoon-hil.com>
2022-09-05 00:34:16 +0200

    Add mp3 to data

--- data.js
+++ data.js
@@ -4,47 +4,47 @@ series = [
         stations: [
             {
                 "name": "Wildstyle",
-                "link": "https://www.dropbox.com/s/2kq2kajq1mh8cym/Wildstyle?raw=1",
+                "link": "https://www.dropbox.com/s/2kq2kajq1mh8cym/Wildstyle?.mp3?raw=1",
                 "image": "https://www.dropbox.com/s/y990damcgb5eqlg/Wildstyle.png?raw=1",
             },
             {
                 "name": "Flash FM",
-                "link": "https://www.dropbox.com/s/ise44wij0b3hcz9/Flash%20FM?raw=1",
+                "link": "https://www.dropbox.com/s/ise44wij0b3hcz9/Flash%20FM?.mp3?raw=1",
                 "image": "https://www.dropbox.com/s/s6f93fh2zluxr3q/Flash%20FM.png?raw=1"
             },
             {
                 "name": "K-Chat",
-                "link": "https://www.dropbox.com/s/6s75bthlipdki16/K-Chat?raw=1",
+                "link": "https://www.dropbox.com/s/6s75bthlipdki16/K-Chat?.mp3?raw=1",
                 "image": "https://www.dropbox.com/s/2n4zixkt6rhlgyx/K-Chat.png?raw=1"
             },
             {
                 "name": "Fever 105",
-                "link": "https://www.dropbox.com/s/n10a0yg71u7gii5/Fever%20105?raw=1",
+                "link": "https://www.dropbox.com/s/n10a0yg71u7gii5/Fever%20105?.mp3?raw=1",
                 "image": "https://www.dropbox.com/s/i22ud8kwo9n4md1/Fever%20105.png?raw=1"
             },
             {
                 "name": "V-Rock",
-                "link": "https://www.dropbox.com/s/6bz1w4yujjodshs/V-Rock?raw=1",
+                "link": "https://www.dropbox.com/s/6bz1w4yujjodshs/V-Rock?.mp3?raw=1",
                 "image": "https://www.dropbox.com/s/wg612luotklq4df/V-Rock.png?raw=1"
             },
             {
                 "name": "VCPR",
-                "link": "https://www.dropbox.com/s/9luvlc4qizw0k66/VCPR?raw=1",
+                "link": "https://www.dropbox.com/s/9luvlc4qizw0k66/VCPR?.mp3?raw=1",
                 "image": "https://www.dropbox.com/s/ehne3woaqvo5c71/VCPR.png?raw=1"
             },
             {
                 "name": "Radio Espantoso",
-                "link": "https://www.dropbox.com/s/006i0txrbtyqrmp/Radio%20Espantoso?raw=1",
+                "link": "https://www.dropbox.com/s/006i0txrbtyqrmp/Radio%20Espantoso?.mp3?raw=1",
                 "image": "https://www.dropbox.com/s/d60ocbiylomw7mh/Radio%20Espantoso.png?raw=1"
             },
             {
                 "name": "Emotion 98.3",
-                "link": "https://www.dropbox.com/s/shekevda4whyxfp/Emotion%2098.3?raw=1",
+                "link": "https://www.dropbox.com/s/shekevda4whyxfp/Emotion%2098.3?.mp3?raw=1",
                 "image": "https://www.dropbox.com/s/sa07k5artmc0wxs/Emotion%2098.3.png?raw=1"
             },
             {
                 "name": "Wave 103",
-                "link": "https://www.dropbox.com/s/n960aggr7pe5gbc/Wave%20103?raw=1",
+                "link": "https://www.dropbox.com/s/n960aggr7pe5gbc/Wave%20103?.mp3?raw=1",
                 "image": "https://www.dropbox.com/s/uy2b2sewgsft7c5/Wave%20103.png?raw=1"
             }
         ]
@@ -54,57 +54,57 @@ series = [
         stations: [
             {
                 "name": "Playback FM",
-                "link": "https://www.dropbox.com/s/c2jcaiw7t9h45k2/Playback%20FM.mp3?raw=1",
+                "link": "https://www.dropbox.com/s/c2jcaiw7t9h45k2/Playback%20FM.mp3?.mp3?raw=1",
                 "image": "https://www.dropbox.com/s/v2lm9vazq104jxw/Playback%20FM.png?raw=1"
             },
             {
                 "name": "K-Rose",
-                "link": "https://www.dropbox.com/s/ag1l19jd0s675u0/K-Rose.mp3?raw=1",
+                "link": "https://www.dropbox.com/s/ag1l19jd0s675u0/K-Rose.mp3?.mp3?raw=1",
                 "image": "https://www.dropbox.com/s/y7d1yj330a5v8p5/K-Rose.png?raw=1"
             },
             {
                 "name": "K-DST",
-                "link": "https://www.dropbox.com/s/0v8mncx7n379xei/K-DST.mp3?raw=1",
+                "link": "https://www.dropbox.com/s/0v8mncx7n379xei/K-DST.mp3?.mp3?raw=1",
                 "image": "https://www.dropbox.com/s/1q22euz08krjaq6/K-DST.png?raw=1"
             },
             {
                 "name": "Bounce FM",
-                "link": "https://www.dropbox.com/s/2bxc5wpx2pl4gt6/Bounce%20FM?raw=1",
+                "link": "https://www.dropbox.com/s/2bxc5wpx2pl4gt6/Bounce%20FM?.mp3?raw=1",
                 "image": "https://www.dropbox.com/s/w11tj8426of8ox7/Bounce%20FM.png?raw=1"
             },
             {
                 "name": "SF-UR",
-                "link": "https://www.dropbox.com/s/hewsirlv1yicrz8/SF-UR.mp3?raw=1",
+                "link": "https://www.dropbox.com/s/hewsirlv1yicrz8/SF-UR.mp3?.mp3?raw=1",
                 "image": "https://www.dropbox.com/s/pkul5pp4m246ex4/SF-UR.png?raw=1"
             },
             {
                 "name": "Radio Los Santos",
-                "link": "https://www.dropbox.com/s/60aixa3vplbze21/Radio%20Los%20Santos.mp3?raw=1",
+                "link": "https://www.dropbox.com/s/60aixa3vplbze21/Radio%20Los%20Santos.mp3?.mp3?raw=1",
                 "image": "https://www.dropbox.com/s/ti9c86ts9a329ai/Radio%20Los%20Santos.png?raw=1"
             },
             {
                 "name": "Radio X",
-                "link": "https://www.dropbox.com/s/s07uc77r61m1kud/Radio%20X.mp3?raw=1",
+                "link": "https://www.dropbox.com/s/s07uc77r61m1kud/Radio%20X.mp3?.mp3?raw=1",
                 "image": "https://www.dropbox.com/s/pqer9hrfw7xm5xg/Radio%20X.png?raw=1"
             },
             {
                 "name": "CSR 103.9",
-                "link": "https://www.dropbox.com/s/ceb0x1vucbvoq72/CSR%20103.9.mp3?raw=1",
+                "link": "https://www.dropbox.com/s/ceb0x1vucbvoq72/CSR%20103.9.mp3?.mp3?raw=1",
                 "image": "https://www.dropbox.com/s/55q8oci33e4qc25/CSR%20103.9.png?raw=1"
             },
             {
                 "name": "K-JAH West",
-                "link": "https://www.dropbox.com/s/lq1xxyaga0oooi9/K-JAH%20West.mp3?raw=1",
+                "link": "https://www.dropbox.com/s/lq1xxyaga0oooi9/K-JAH%20West.mp3?.mp3?raw=1",
                 "image": "https://www.dropbox.com/s/gccrq6xu2yvvp4q/K-JAH%20West.png?raw=1"
             },
             {
                 "name": "Master Sounds 98.3",
-                "link": "https://www.dropbox.com/s/rjj2saeam9comg7/Master%20Sounds%2098.3.mp3?raw=1",
+                "link": "https://www.dropbox.com/s/rjj2saeam9comg7/Master%20Sounds%2098.3.mp3?.mp3?raw=1",
                 "image": "https://www.dropbox.com/s/8g0nhpsjg22tpzx/Master%20Sounds%2098.3.png?raw=1"
             },
             {
                 "name": "WCTR",
-                "link": "https://www.dropbox.com/s/y1zo07omkypor9h/WCTR.mp3?raw=1",
+                "link": "https://www.dropbox.com/s/y1zo07omkypor9h/WCTR.mp3?.mp3?raw=1",
                 "image": "https://www.dropbox.com/s/xzs891xef1qjjud/WCTR.png?raw=1"
             }
         ]
@@ -114,87 +114,87 @@ series = [
         stations: [
             {
                 "name": "Los Santos",
-                "link": "https://www.dropbox.com/s/35xxqd8hrj95x8g/Los%20Santos?raw=1",
+                "link": "https://www.dropbox.com/s/35xxqd8hrj95x8g/Los%20Santos?.mp3?raw=1",
                 "image": "https://www.dropbox.com/s/cfgtm3o52olzgx0/Los%20Santos.png?raw=1"
             },
             {
                 "name": "Space 103.2",
-                "link": "https://www.dropbox.com/s/g4m76yg3sjyt2wh/Space%20103.2?raw=1",
+                "link": "https://www.dropbox.com/s/g4m76yg3sjyt2wh/Space%20103.2?.mp3?raw=1",
                 "image": "https://www.dropbox.com/s/h6emf6o2og7gfs5/Space%20103.2.png?raw=1"
             },
             {
                 "name": "West Coast Classics",
-                "link": "https://www.dropbox.com/s/599ya67np2ge0cv/West%20Coast%20Classics.mp3?raw=1",
+                "link": "https://www.dropbox.com/s/599ya67np2ge0cv/West%20Coast%20Classics.mp3?.mp3?raw=1",
                 "image": "https://www.dropbox.com/s/6rzwbe40ofwxmfc/West%20Coast%20Classics.png?raw=1"
             },
             {
                 "name": "Rebel Radio",
-                "link": "https://www.dropbox.com/s/236e2kxazxi6fyv/Rebel%20Radio?raw=1",
+                "link": "https://www.dropbox.com/s/236e2kxazxi6fyv/Rebel%20Radio?.mp3?raw=1",
                 "image": "https://www.dropbox.com/s/lyfg258rne8bz1x/Rebel%20Radio.png?raw=1"
             },
             {
                 "name": "Los Santos Rock Radio",
-                "link": "https://www.dropbox.com/s/bxfqh236axpq74n/Los%20Santos%20Rock%20Radio?raw=1",
+                "link": "https://www.dropbox.com/s/bxfqh236axpq74n/Los%20Santos%20Rock%20Radio?.mp3?raw=1",
                 "image": "https://www.dropbox.com/s/2tutw2tlzyqtejx/Los%20Santos%20Rock%20Radio.png?raw=1"
             },
             {
                 "name": "The Lowdown 91.1",
-                "link": "https://www.dropbox.com/s/59q6dqkfngblhsr/The%20Lowdown%2091.1.mp3?raw=1",
+                "link": "https://www.dropbox.com/s/59q6dqkfngblhsr/The%20Lowdown%2091.1.mp3?.mp3?raw=1",
                 "image": "https://www.dropbox.com/s/w4guu3bmubvicqs/The%20Lowdown%2091.1.png?raw=1"
             },
             {
                 "name": "The Blue Ark",
-                "link": "https://www.dropbox.com/s/352oslrejmdk0b0/The%20Blue%20Ark?raw=1",
+                "link": "https://www.dropbox.com/s/352oslrejmdk0b0/The%20Blue%20Ark?.mp3?raw=1",
                 "image": "https://www.dropbox.com/s/4nvlymu34x2q0kt/The%20Blue%20Ark.png?raw=1"
             },
             {
                 "name": "Non-Stop-Pop FM",
-                "link": "https://www.dropbox.com/s/i7ina90i9p00r2k/Non-Stop-Pop%20FM?raw=1",
+                "link": "https://www.dropbox.com/s/i7ina90i9p00r2k/Non-Stop-Pop%20FM?.mp3?raw=1",
                 "image": "https://www.dropbox.com/s/c5byatff92n2fdm/Non-Stop-Pop%20FM.png?raw=1"
             },
             {
                 "name": "East Los FM",
-                "link": "https://www.dropbox.com/s/5z4lnxqy0edx9zq/East%20Los%20FM?raw=1",
+                "link": "https://www.dropbox.com/s/5z4lnxqy0edx9zq/East%20Los%20FM?.mp3?raw=1",
                 "image": "https://www.dropbox.com/s/dear0nvz9nv52lp/East%20Los%20FM.png?raw=1"
             },
             {
                 "name": "Channel X",
-                "link": "https://www.dropbox.com/s/5eiwb8qylbws4qz/Channel%20X?raw=1",
+                "link": "https://www.dropbox.com/s/5eiwb8qylbws4qz/Channel%20X?.mp3?raw=1",
                 "image": "https://www.dropbox.com/s/egjssgo9sax1wss/Channel%20X.png?raw=1"
             },
             {
                 "name": "Radio Mirror Park",
-                "link": "https://www.dropbox.com/s/rkanxyqdowny9fp/Radio%20Mirror%20Park?raw=1",
+                "link": "https://www.dropbox.com/s/rkanxyqdowny9fp/Radio%20Mirror%20Park?.mp3?raw=1",
                 "image": "https://www.dropbox.com/s/9wd0fq7hrv9ne0g/Radio%20Mirror%20Park.png?raw=1"
             },
             {
                 "name": "Vinewood Boulevard Radio",
-                "link": "https://www.dropbox.com/s/xdkydeney9tovle/Vinewood%20Boulevard%20Radio?raw=1",
+                "link": "https://www.dropbox.com/s/xdkydeney9tovle/Vinewood%20Boulevard%20Radio?.mp3?raw=1",
                 "image": "https://www.dropbox.com/s/gusanezga8zzzq8/Vinewood%20Boulevard%20Radio.png?raw=1"
             },
             {
                 "name": "Soulwax FM",
-                "link": "https://www.dropbox.com/s/miv75zp2dhrh3qx/Soulwax%20FM?raw=1",
+                "link": "https://www.dropbox.com/s/miv75zp2dhrh3qx/Soulwax%20FM?.mp3?raw=1",
                 "image": "https://www.dropbox.com/s/n5e8td61e284ab8/Soulwax%20FM.png?raw=1"
             },
             {
                 "name": "FlyLo FM",
-                "link": "https://www.dropbox.com/s/n1cfozlvxrykgwd/FlyLo%20FM?raw=1",
+                "link": "https://www.dropbox.com/s/n1cfozlvxrykgwd/FlyLo%20FM?.mp3?raw=1",
                 "image": "https://www.dropbox.com/s/h1zuazqvhg1u4rv/FlyLo%20FM.png?raw=1"
             },
             {
                 "name": "Blaine County Talk Radio",
-                "link": "https://www.dropbox.com/s/52660lvb9y75qgr/Blaine%20County%20Talk%20Radio?raw=1",
+                "link": "https://www.dropbox.com/s/52660lvb9y75qgr/Blaine%20County%20Talk%20Radio?.mp3?raw=1",
                 "image": "https://www.dropbox.com/s/ad3c7opawraexpx/Blaine%20County%20Talk%20Radio.png?raw=1"
             },
             {
                 "name": "WCTR",
-                "link": "https://www.dropbox.com/s/kwbmgpp5rmf09yc/WCTR?raw=1",
+                "link": "https://www.dropbox.com/s/kwbmgpp5rmf09yc/WCTR?.mp3?raw=1",
                 "image": "https://www.dropbox.com/s/m6egrfg48t8z29g/WCTR.png?raw=1"
             },
             {
                 "name": "The Lab",
-                "link": "https://www.dropbox.com/s/day4fotwnpz3m1o/The%20Lab?raw=1",
+                "link": "https://www.dropbox.com/s/day4fotwnpz3m1o/The%20Lab?.mp3?raw=1",
                 "image": "https://www.dropbox.com/s/mk7wiv2dq86d06p/The%20Lab.png?raw=1"
             }
         ]
