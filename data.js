10a244e8258a53a2bd6b21b1ef4cc2740f630b72
Nenad Misic <nenad.misic@typhoon-hil.com>
2022-09-04 17:05:14 +0200

    Shift images to cloud solution

--- data.js
+++ data.js
@@ -4,39 +4,48 @@ series = [
         stations: [
             {
                 "name": "Wildstyle",
-                "link": "https://dl.dropboxusercontent.com/s/2kq2kajq1mh8cym/Wildstyle?dl=0"
+                "link": "https://dl.dropboxusercontent.com/s/2kq2kajq1mh8cym/Wildstyle?dl=0",
+                "image": "https://dl.dropboxusercontent.com/s/y990damcgb5eqlg/Wildstyle.png?dl=0",
             },
             {
                 "name": "Flash FM",
-                "link": "https://dl.dropboxusercontent.com/s/ise44wij0b3hcz9/Flash%20FM?dl=0"
+                "link": "https://dl.dropboxusercontent.com/s/ise44wij0b3hcz9/Flash%20FM?dl=0",
+                "image": "https://dl.dropboxusercontent.com/s/s6f93fh2zluxr3q/Flash%20FM.png?dl=0"
             },
             {
                 "name": "K-Chat",
-                "link": "https://dl.dropboxusercontent.com/s/6s75bthlipdki16/K-Chat?dl=0"
+                "link": "https://dl.dropboxusercontent.com/s/6s75bthlipdki16/K-Chat?dl=0",
+                "image": "https://dl.dropboxusercontent.com/s/2n4zixkt6rhlgyx/K-Chat.png?dl=0"
             },
             {
                 "name": "Fever 105",
-                "link": "https://dl.dropboxusercontent.com/s/n10a0yg71u7gii5/Fever%20105?dl=0"
+                "link": "https://dl.dropboxusercontent.com/s/n10a0yg71u7gii5/Fever%20105?dl=0",
+                "image": "https://dl.dropboxusercontent.com/s/i22ud8kwo9n4md1/Fever%20105.png?dl=0"
             },
             {
                 "name": "V-Rock",
-                "link": "https://dl.dropboxusercontent.com/s/6bz1w4yujjodshs/V-Rock?dl=0"
+                "link": "https://dl.dropboxusercontent.com/s/6bz1w4yujjodshs/V-Rock?dl=0",
+                "image": "https://dl.dropboxusercontent.com/s/wg612luotklq4df/V-Rock.png?dl=0"
             },
             {
                 "name": "VCPR",
-                "link": "https://dl.dropboxusercontent.com/s/9luvlc4qizw0k66/VCPR?dl=0"
+                "link": "https://dl.dropboxusercontent.com/s/9luvlc4qizw0k66/VCPR?dl=0",
+                "image": "https://dl.dropboxusercontent.com/s/ehne3woaqvo5c71/VCPR.png?dl=0"
             },
             {
                 "name": "Radio Espantoso",
-                "link": "https://dl.dropboxusercontent.com/s/006i0txrbtyqrmp/Radio%20Espantoso?dl=0"
+                "link": "https://dl.dropboxusercontent.com/s/006i0txrbtyqrmp/Radio%20Espantoso?dl=0",
+                "image": "https://dl.dropboxusercontent.com/s/d60ocbiylomw7mh/Radio%20Espantoso.png?dl=0"
             },
             {
                 "name": "Emotion 98.3",
-                "link": "https://dl.dropboxusercontent.com/s/shekevda4whyxfp/Emotion%2098.3?dl=0"
+                "link": "https://dl.dropboxusercontent.com/s/shekevda4whyxfp/Emotion%2098.3?dl=0",
+                "image": "https://dl.dropboxusercontent.com/s/sa07k5artmc0wxs/Emotion%2098.3.png?dl=0"
             },
             {
                 "name": "Wave 103",
-                "link": "https://dl.dropboxusercontent.com/s/n960aggr7pe5gbc/Wave%20103?dl=0"
+                "link": "https://dl.dropboxusercontent.com/s/n960aggr7pe5gbc/Wave%20103?dl=0",
+                "image": "https://dl.dropboxusercontent.com/s/uy2b2sewgsft7c5/Wave%20103.png?dl=0"
             }
         ]
     },
@@ -45,47 +54,58 @@ series = [
         stations: [
             {
                 "name": "Playback FM",
-                "link": ""
+                "link": "https://dl.dropboxusercontent.com/s/c2jcaiw7t9h45k2/Playback%20FM.mp3?dl=0",
+                "image": "https://dl.dropboxusercontent.com/s/v2lm9vazq104jxw/Playback%20FM.png?dl=0"
             },
             {
                 "name": "K-Rose",
-                "link": ""
+                "link": "https://dl.dropboxusercontent.com/s/ag1l19jd0s675u0/K-Rose.mp3?dl=0",
+                "image": "https://dl.dropboxusercontent.com/s/y7d1yj330a5v8p5/K-Rose.png?dl=0"
             },
             {
                 "name": "K-DST",
-                "link": ""
+                "link": "https://dl.dropboxusercontent.com/s/0v8mncx7n379xei/K-DST.mp3?dl=0",
+                "image": "https://dl.dropboxusercontent.com/s/1q22euz08krjaq6/K-DST.png?dl=0"
             },
             {
                 "name": "Bounce FM",
-                "link": "https://dl.dropboxusercontent.com/s/2bxc5wpx2pl4gt6/Bounce%20FM?dl=0"
+                "link": "https://dl.dropboxusercontent.com/s/2bxc5wpx2pl4gt6/Bounce%20FM?dl=0",
+                "image": "https://dl.dropboxusercontent.com/s/w11tj8426of8ox7/Bounce%20FM.png?dl=0"
             },
             {
                 "name": "SF-UR",
-                "link": ""
+                "link": "https://dl.dropboxusercontent.com/s/hewsirlv1yicrz8/SF-UR.mp3?dl=0",
+                "image": "https://dl.dropboxusercontent.com/s/pkul5pp4m246ex4/SF-UR.png?dl=0"
             },
             {
                 "name": "Radio Los Santos",
-                "link": ""
+                "link": "https://dl.dropboxusercontent.com/s/60aixa3vplbze21/Radio%20Los%20Santos.mp3?dl=0",
+                "image": "https://dl.dropboxusercontent.com/s/ti9c86ts9a329ai/Radio%20Los%20Santos.png?dl=0"
             },
             {
                 "name": "Radio X",
-                "link": ""
+                "link": "https://dl.dropboxusercontent.com/s/s07uc77r61m1kud/Radio%20X.mp3?dl=0",
+                "image": "https://dl.dropboxusercontent.com/s/pqer9hrfw7xm5xg/Radio%20X.png?dl=0"
             },
             {
                 "name": "CSR 103.9",
-                "link": ""
+                "link": "https://dl.dropboxusercontent.com/s/ceb0x1vucbvoq72/CSR%20103.9.mp3?dl=0",
+                "image": "https://dl.dropboxusercontent.com/s/55q8oci33e4qc25/CSR%20103.9.png?dl=0"
             },
             {
                 "name": "K-JAH West",
-                "link": ""
+                "link": "https://dl.dropboxusercontent.com/s/lq1xxyaga0oooi9/K-JAH%20West.mp3?dl=0",
+                "image": "https://dl.dropboxusercontent.com/s/gccrq6xu2yvvp4q/K-JAH%20West.png?dl=0"
             },
             {
                 "name": "Master Sounds 98.3",
-                "link": ""
+                "link": "https://dl.dropboxusercontent.com/s/rjj2saeam9comg7/Master%20Sounds%2098.3.mp3?dl=0",
+                "image": "https://dl.dropboxusercontent.com/s/8g0nhpsjg22tpzx/Master%20Sounds%2098.3.png?dl=0"
             },
             {
                 "name": "WCTR",
-                "link": ""
+                "link": "https://dl.dropboxusercontent.com/s/y1zo07omkypor9h/WCTR.mp3?dl=0",
+                "image": "https://dl.dropboxusercontent.com/s/xzs891xef1qjjud/WCTR.png?dl=0"
             }
         ]
     },
@@ -94,75 +114,88 @@ series = [
         stations: [
             {
                 "name": "Los Santos",
-                "link": "https://dl.dropboxusercontent.com/s/35xxqd8hrj95x8g/Los%20Santos?dl=0"
+                "link": "https://dl.dropboxusercontent.com/s/35xxqd8hrj95x8g/Los%20Santos?dl=0",
+                "image": "https://dl.dropboxusercontent.com/s/cfgtm3o52olzgx0/Los%20Santos.png?dl=0"
             },
             {
                 "name": "Space 103.2",
-                "link": "https://dl.dropboxusercontent.com/s/g4m76yg3sjyt2wh/Space%20103.2?dl=0"
+                "link": "https://dl.dropboxusercontent.com/s/g4m76yg3sjyt2wh/Space%20103.2?dl=0",
+                "image": "https://dl.dropboxusercontent.com/s/h6emf6o2og7gfs5/Space%20103.2.png?dl=0"
             },
             {
                 "name": "West Coast Classics",
-                "link": ""
+                "link": "https://dl.dropboxusercontent.com/s/599ya67np2ge0cv/West%20Coast%20Classics.mp3?dl=0",
+                "image": "https://dl.dropboxusercontent.com/s/6rzwbe40ofwxmfc/West%20Coast%20Classics.png?dl=0"
             },
             {
                 "name": "Rebel Radio",
-                "link": "https://dl.dropboxusercontent.com/s/236e2kxazxi6fyv/Rebel%20Radio?dl=0"
+                "link": "https://dl.dropboxusercontent.com/s/236e2kxazxi6fyv/Rebel%20Radio?dl=0",
+                "image": "https://dl.dropboxusercontent.com/s/lyfg258rne8bz1x/Rebel%20Radio.png?dl=0"
             },
             {
                 "name": "Los Santos Rock Radio",
-                "link": "https://dl.dropboxusercontent.com/s/bxfqh236axpq74n/Los%20Santos%20Rock%20Radio?dl=0"
+                "link": "https://dl.dropboxusercontent.com/s/bxfqh236axpq74n/Los%20Santos%20Rock%20Radio?dl=0",
+                "image": "https://dl.dropboxusercontent.com/s/2tutw2tlzyqtejx/Los%20Santos%20Rock%20Radio.png?dl=0"
             },
             {
                 "name": "The Lowdown 91.1",
-                "link": ""
+                "link": "https://dl.dropboxusercontent.com/s/59q6dqkfngblhsr/The%20Lowdown%2091.1.mp3?dl=0",
+                "image": "https://dl.dropboxusercontent.com/s/w4guu3bmubvicqs/The%20Lowdown%2091.1.png?dl=0"
             },
             {
                 "name": "The Blue Ark",
-                "link": "https://dl.dropboxusercontent.com/s/352oslrejmdk0b0/The%20Blue%20Ark?dl=0"
+                "link": "https://dl.dropboxusercontent.com/s/352oslrejmdk0b0/The%20Blue%20Ark?dl=0",
+                "image": "https://dl.dropboxusercontent.com/s/4nvlymu34x2q0kt/The%20Blue%20Ark.png?dl=0"
             },
             {
                 "name": "Non-Stop-Pop FM",
-                "link": "https://dl.dropboxusercontent.com/s/i7ina90i9p00r2k/Non-Stop-Pop%20FM?dl=0"
+                "link": "https://dl.dropboxusercontent.com/s/i7ina90i9p00r2k/Non-Stop-Pop%20FM?dl=0",
+                "image": "https://dl.dropboxusercontent.com/s/c5byatff92n2fdm/Non-Stop-Pop%20FM.png?dl=0"
             },
             {
                 "name": "East Los FM",
-                "link": "https://dl.dropboxusercontent.com/s/5z4lnxqy0edx9zq/East%20Los%20FM?dl=0"
-            },
-            {
-                "name": "WorldWide FM",
-                "link": ""
+                "link": "https://dl.dropboxusercontent.com/s/5z4lnxqy0edx9zq/East%20Los%20FM?dl=0",
+                "image": "https://dl.dropboxusercontent.com/s/dear0nvz9nv52lp/East%20Los%20FM.png?dl=0"
             },
             {
                 "name": "Channel X",
-                "link": "https://dl.dropboxusercontent.com/s/5eiwb8qylbws4qz/Channel%20X?dl=0"
+                "link": "https://dl.dropboxusercontent.com/s/5eiwb8qylbws4qz/Channel%20X?dl=0",
+                "image": "https://dl.dropboxusercontent.com/s/egjssgo9sax1wss/Channel%20X.png?dl=0"
             },
             {
                 "name": "Radio Mirror Park",
-                "link": "https://dl.dropboxusercontent.com/s/rkanxyqdowny9fp/Radio%20Mirror%20Park?dl=0"
+                "link": "https://dl.dropboxusercontent.com/s/rkanxyqdowny9fp/Radio%20Mirror%20Park?dl=0",
+                "image": "https://dl.dropboxusercontent.com/s/9wd0fq7hrv9ne0g/Radio%20Mirror%20Park.png?dl=0"
             },
             {
                 "name": "Vinewood Boulevard Radio",
-                "link": "https://dl.dropboxusercontent.com/s/xdkydeney9tovle/Vinewood%20Boulevard%20Radio?dl=0"
+                "link": "https://dl.dropboxusercontent.com/s/xdkydeney9tovle/Vinewood%20Boulevard%20Radio?dl=0",
+                "image": "https://dl.dropboxusercontent.com/s/gusanezga8zzzq8/Vinewood%20Boulevard%20Radio.png?dl=0"
             },
             {
                 "name": "Soulwax FM",
-                "link": "https://dl.dropboxusercontent.com/s/miv75zp2dhrh3qx/Soulwax%20FM?dl=0"
+                "link": "https://dl.dropboxusercontent.com/s/miv75zp2dhrh3qx/Soulwax%20FM?dl=0",
+                "image": "https://dl.dropboxusercontent.com/s/n5e8td61e284ab8/Soulwax%20FM.png?dl=0"
             },
             {
                 "name": "FlyLo FM",
-                "link": "https://dl.dropboxusercontent.com/s/n1cfozlvxrykgwd/FlyLo%20FM?dl=0"
+                "link": "https://dl.dropboxusercontent.com/s/n1cfozlvxrykgwd/FlyLo%20FM?dl=0",
+                "image": "https://dl.dropboxusercontent.com/s/h1zuazqvhg1u4rv/FlyLo%20FM.png?dl=0"
             },
             {
                 "name": "Blaine County Talk Radio",
-                "link": "https://dl.dropboxusercontent.com/s/52660lvb9y75qgr/Blaine%20County%20Talk%20Radio?dl=0"
+                "link": "https://dl.dropboxusercontent.com/s/52660lvb9y75qgr/Blaine%20County%20Talk%20Radio?dl=0",
+                "image": "https://dl.dropboxusercontent.com/s/ad3c7opawraexpx/Blaine%20County%20Talk%20Radio.png?dl=0"
             },
             {
                 "name": "WCTR",
-                "link": "https://dl.dropboxusercontent.com/s/kwbmgpp5rmf09yc/WCTR?dl=0"
+                "link": "https://dl.dropboxusercontent.com/s/kwbmgpp5rmf09yc/WCTR?dl=0",
+                "image": "https://dl.dropboxusercontent.com/s/m6egrfg48t8z29g/WCTR.png?dl=0"
             },
             {
                 "name": "The Lab",
-                "link": "https://dl.dropboxusercontent.com/s/day4fotwnpz3m1o/The%20Lab?dl=0"
+                "link": "https://dl.dropboxusercontent.com/s/day4fotwnpz3m1o/The%20Lab?dl=0",
+                "image": "https://dl.dropboxusercontent.com/s/mk7wiv2dq86d06p/The%20Lab.png?dl=0"
             }
         ]
     },
--- index.js
+++ index.js
@@ -2,6 +2,13 @@ let slides_per_view = 5;
 let series_len = series.map(e => e.stations.length)
 let total_len = series_len.reduce((partialSum, a) => partialSum + a, 0);
 let series_flattened = series.map(serie => serie.stations).flat();
+let preloaded = false;
+
+
+function ons() {
+    document.getElementById('overlay_loading').style.opacity = 0;
+    setTimeout(() => document.getElementById('overlay_loading').style.display = "none", 500)
+}
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -10,24 +17,23 @@ function sleep(ms) {
 
 for (let serie of series) {
     for (let station of serie.stations) {
+        // document.getElementById('swiper-wrapper').innerHTML += `
+        // <div class="swiper-slide">
+        // <img src="image_output/${serie.name}/${station.name}.png">
+        // </div>
+        // `
         document.getElementById('swiper-wrapper').innerHTML += `
         <div class="swiper-slide">
-        <img src="image_output/${serie.name}/${station.name}.png">
+        <img src="${station.image}">
         </div>
         `
-        if (!station.link) {
-            document.getElementById('audios').innerHTML += `
-            <audio controls style="display: none;" id="audio_${station.name}"  preload="none">
-                <source src="https://dl.dropboxusercontent.com/s/ise44wij0b3hcz9/Flash%20FM?dl=0" type="audio/mpeg" />
-            </audio>
-            `
-        } else {
-            document.getElementById('audios').innerHTML += `
-            <audio controls style="display: none;" id="audio_${station.name}"  preload="none">
-                <source src="${station.link}" type="audio/mpeg" />
-            </audio>
-            `
-        }
+
+        document.getElementById('audios').innerHTML += `
+        <audio controls style="display: none;" id="audio_${station.name}"  preload="none" onseeked="ons()">
+            <source src="${station.link}" type="audio/mpeg" />
+        </audio>
+        `
+
 
     }
 
@@ -41,8 +47,12 @@ var swiper = new Swiper(".swiper", {
 });
     
 swiper.on('activeIndexChange', async function () {
+    document.querySelector("body").setAttribute("scroll", "false");
+
     let active = swiper.activeIndex
-    let preload_list = [active - 3, active - 2, active -1, active + 1, active + 2, active + 3, active + 4]
+    // let preload_list = [...new Array(series_flattened.length)].map((e, i) => i);
+    let preload_list = [];
+
     localStorage.setItem("last_index", active)
 
     let mapped_active = active >= total_len ? active - total_len : active;
@@ -50,24 +60,17 @@ swiper.on('activeIndexChange', async function () {
     shifted_active += shifted_active < 0 ? total_len : 0;
     let current_active_object = series_flattened[shifted_active];
 
-    let mapped_preload = preload_list.map(i => i >= total_len ? i - total_len : i);
-    let shifted_preload = mapped_preload.map(i => i - slides_per_view);
-    shifted_preload = shifted_preload.map(i => i + (i < 0 ? total_len : 0));
-    let preload_objects = shifted_preload.map(i => series_flattened[i]);
-
     for (let last_audio of document.querySelectorAll('audio')) {
         last_audio.pause();
-    }
-    
-    for (let preload_object of preload_objects) {
-        let audio_for_preload = document.getElementById(`audio_${preload_object.name}`)
-        audio_for_preload.load();
     }
 
     let audio = document.getElementById(`audio_${current_active_object.name}`)
     audio.play();
 
     let wait_counter = 0;
+    document.getElementById('overlay_loading').style.display = "block";
+    setTimeout(() => {document.getElementById('overlay_loading').style.opacity = 1;}, 30);
+    
     while (!audio.duration) {
         wait_counter += 1;
         await sleep(5);
@@ -75,13 +78,34 @@ swiper.on('activeIndexChange', async function () {
             break;
         }
     }
+
     let station_duration = Math.floor(audio.duration);
     let current_millis = (new Date()).getTime();
     let current_seconds = Math.floor(current_millis / 1000);
     let calculated_time = current_seconds % station_duration;
+
     audio.currentTime = calculated_time?calculated_time:0;
+    
+    
+    if (!preloaded) {
+        let mapped_preload = preload_list.map(i => i >= total_len ? i - total_len : i);
+        let shifted_preload = mapped_preload.map(i => i - slides_per_view);
+        shifted_preload = shifted_preload.map(i => i + (i < 0 ? total_len : 0));
+        let preload_objects = shifted_preload.map(i => series_flattened[i]);
+        preloaded = true;
+        for (let preload_object of preload_objects) {
+            if (preload_object.name != current_active_object.name){ 
+                let audio_for_preload = document.getElementById(`audio_${preload_object.name}`)
+                audio_for_preload.load();
+            }
+        }
+    }
 
  });
+
+ swiper.on('sliderFirstMove', () => document.querySelector("body").setAttribute("scroll", "true"))
+
+
 let last_user_index = localStorage.getItem("last_index");
 if (!last_user_index) {
     last_user_index = 15; //K Rose, ofc
